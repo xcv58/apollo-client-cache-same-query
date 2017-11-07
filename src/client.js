e1a45319f3c45a5d81563ed82d9d611e4cd7a5dd
Yihong Chen <yihongc@twitter.com>
2017-11-07 11:54:59 -0800

    Add DedupLink

--- src/client.js
+++ src/client.js
@@ -1,13 +1,19 @@
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { DedupLink } from "apollo-link-dedup"
+import { ApolloLink } from 'apollo-link'
 
-const link = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn',
-  fetchOptions: {
-    credentials: 'include'
-  }
-})
+
+const link = ApolloLink.from([
+  new DedupLink(),
+  new HttpLink({
+    uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn',
+    fetchOptions: {
+      credentials: 'include'
+    }
+  })
+])
 
 const cache = new InMemoryCache()
 
